5168e70f193eb5a04556b14eb8b4e59f09376850
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2021-03-10 00:54:33 +0900

    Matching much more

--- src/milenage.erl
+++ src/milenage.erl
@@ -72,24 +72,17 @@ f1star(M, SQN, AMF) ->
     <<MACS:64>>.
 
 %% Performs the calcurations that are common in f1/1 and f1star/1.
-f1base(M, SQN, AMF) ->
-    K = M#milenage.k,
-    OPc = M#milenage.opc,
-
+f1base(#milenage{k = K, opc = OPc, rand = RAND}, SQN, AMF) ->
     <<Rx8F:64, Rx07:64>> = crypto:exor(<<SQN:48, AMF:16, SQN:48, AMF:16>>, OPc),
-
-    T = encrypt(K, crypto:exor(M#milenage.rand, OPc)),
+    T = encrypt(K, crypto:exor(RAND, OPc)),
     O = encrypt(K, crypto:exor(<<Rx07:64, Rx8F:64>>, T)),
     crypto:exor(O, OPc).
 
 %% Performas the functions f2, f3, f4, f5 at a time which take key K and random
 %% challenge RAND, and returns response RES, confidentiality key CK, integrity key
 %% IK and anonymity key AK.
-f2345(M) ->
-    K = M#milenage.k,
-    OPc = M#milenage.opc,
-
-    Temp = encrypt(K, crypto:exor(M#milenage.rand, OPc)),
+f2345(#milenage{k = K, opc = OPc, rand = RAND}) ->
+    Temp = encrypt(K, crypto:exor(RAND, OPc)),
     R1 = crypto:exor(Temp, OPc),
     <<_:120, R1x:8>> = R1,
     X1 = R1x bxor 1,
@@ -112,11 +105,8 @@ f2345(M) ->
 %% Performs f5 star which is the anonymity key derivation function for the
 %% re-synchronisation message. It takes key K and random challenge RAND, and
 %% returns resynch anonymity key AK.
-f5star(M) ->
-    K = M#milenage.k,
-    OPc = M#milenage.opc,
-
-    Temp = encrypt(K, crypto:exor(M#milenage.rand, OPc)),
+f5star(#milenage{k = K, opc = OPc, rand = RAND}) ->
+    Temp = encrypt(K, crypto:exor(RAND, OPc)),
     <<Rx4F:96, Rx03:32>> = crypto:exor(Temp, OPc),
     R = <<Rx03:32, Rx4F:96>>,
     <<_:120, Rx:8>> = R,
@@ -126,16 +116,16 @@ f5star(M) ->
 
 %% Performs all the milenage functions and returns the milenage record
 %% with the computed values set.
-compute_all(M) ->
-    MACA = f1(M, M#milenage.sqn, M#milenage.amf),
-    MACS = f1star(M, M#milenage.sqn, 0),
+compute_all(#milenage{sqn = SQN, amf = AMF} = M) ->
+    MACA = f1(M, SQN, AMF),
+    MACS = f1star(M, SQN, 0),
     {RES, CK, IK, AK} = f2345(M),
     AKS = f5star(M),
-    M#milenage{mac_a=MACA, mac_s=MACS, res=RES, ck=CK, ik=IK, ak=AK, aks=AKS}.
+    M#milenage{mac_a = MACA, mac_s = MACS, res = RES, ck = CK, ik = IK, ak = AK, aks = AKS}.
 
 %% Performs RES* derivation function which is defined in A.4 RES* and XRES*
 %% derivation function, TS 33.501.
-compute_res_star(#milenage{rand = Rand, res = Res, ck = CK, ik = IK}, MCC, MNC) when length(MCC) =:= 3 ->
+compute_res_star(#milenage{rand = RAND, res = Res, ck = CK, ik = IK}, MCC, MNC) when length(MCC) =:= 3 ->
     N = case length(MNC) of
         2 -> "0" ++ MNC;
         3 -> MNC;
@@ -143,7 +133,7 @@ compute_res_star(#milenage{rand = Rand, res = Res, ck = CK, ik = IK}, MCC, MNC) 
     end,
 
     SNN = list_to_binary(lists:flatten(io_lib:format("5G:mnc~s.mcc~s.3gppnetwork.org", [N, MCC]))),
-    B = <<16#6b, SNN:32/binary, 32:16, Rand:16/binary, 16:16, Res:8/binary, 8:16>>,
+    B = <<16#6b, SNN:32/binary, 32:16, RAND:16/binary, 16:16, Res:8/binary, 8:16>>,
 
     <<_:128, Out/binary>> = crypto:mac(hmac, sha256, <<CK:16/binary, IK:16/binary>>, B),
     Out.
